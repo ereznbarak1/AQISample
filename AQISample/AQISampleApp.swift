64e1eb9a500ce13c26912836fa4838a660303e1c
erezbarak1 <ereznbarak@gmail.com>
2021-12-30 20:40:58 +0200

    Initial Commit

---
+++ AQISample/AQISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQISampleApp.swift
+//  AQISample
+//
+//  Created by Erez Barak on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AQISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
